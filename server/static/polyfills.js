93a340cf697ab02fd89b6e27f3e21de4d445fde0
hizir_aslan <hzraslan@gmail.com>
2018-07-10 23:36:07 -0700

    make it

--- server/static/polyfills.js
+++ server/static/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/hizir/Desktop/DojoAssignments/MEAN/angular/projects/Website1/public/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/hizir/Desktop/DojoAssignments/MEAN/angular/projects/interact/public/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- server/static/styles.js
+++ server/static/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/hizir/Desktop/DojoAssignments/MEAN/angular/projects/Website1/public/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /Users/hizir/Desktop/DojoAssignments/MEAN/angular/projects/interact/public/src/styles.css */"./src/styles.css");
 
 
 /***/ })
